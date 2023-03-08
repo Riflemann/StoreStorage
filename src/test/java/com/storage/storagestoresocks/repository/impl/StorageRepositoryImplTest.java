77543a0831d5e1d417bbbf08279ae50bcdf01ed0
Konstantin <kdanilov1911@gmail.com>
2023-03-09 02:07:46 +0300

    in progress

---
+++ src/test/java/com/storage/storagestoresocks/repository/impl/StorageRepositoryImplTest.java
@@ -0,0 +1,16 @@
+package com.storage.storagestoresocks.repository.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StorageRepositoryImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void batchUpdate() {
+    }
+}
